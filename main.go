22f9e1e4c78527b8616069786e8f9c8082789876
AnjasFedo <fedoaf98@gmail.com>
2024-01-15 19:26:42 +0700

    Command Definition

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
 	"os"
-	"bufio"
+	"strconv"
 	"strings"
 
 	"github.com/shomali11/slacker"
 )
 
 func main() {
+	if err := loadEnv(); err != nil {
+		log.Fatal("Error on Load .env:", err)
+	}
 
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Examples:    []string{"my yob is 2020"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				response.Reply("Invalid year format. Please provide a valid year.")
+				return
+			}
+
+			age := 2024 - yob
+
+			r := fmt.Sprintf("Age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func loadEnv() error {
@@ -40,4 +73,15 @@ func loadEnv() error {
 	}
 
 	return nil
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
 }
